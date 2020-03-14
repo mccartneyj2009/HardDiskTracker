0023256b10539829cac1569e90af6c1efc68cd4e
James McCartney <mccartneyj2009@gmail.com>
2020-03-14 14:30:28 -0400

    First Commit.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardDiskTracker
+{
+    class DriveInformation
+    {
+        static void Main()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drv in allDrives)
+            {
+                double gbConversion = 1073741824;
+                double gbDiskAvail = drv.AvailableFreeSpace;
+
+                int diskAvail = Convert.ToInt32(gbDiskAvail / gbConversion);
+
+
+                Console.WriteLine(drv.Name);
+                Console.WriteLine(diskAvail);
+                Console.WriteLine(drv.TotalFreeSpace + " Bytes");
+                Console.Write(drv.TotalSize + " Bytes");
+                Console.ReadKey();
+            }
+        }
+    }
+}
