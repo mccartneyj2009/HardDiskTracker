a18e77682b2ae557d7fe0074347f4605589c4181
James McCartney <mccartneyj2009@gmail.com>
2020-03-17 22:57:12 -0400

    Changes to what is displayed in the console.

--- Program.cs
+++ Program.cs
@@ -9,25 +9,26 @@ namespace HardDiskTracker
 {
     class DriveInformation
     {
-        static void Main()
+        static void Main(string[] args)
         {
             DriveInfo[] allDrives = DriveInfo.GetDrives();
 
             foreach (DriveInfo drv in allDrives)
             {
-                double gbConversion = 1073741824;
+               const double gbConversion = 1073741824;
                 double gbDiskAvail = drv.AvailableFreeSpace;
+                double gbDiskStart = drv.TotalSize;
                 string type = Convert.ToString(drv.DriveType);
                 string name = drv.Name;
                 string label = drv.VolumeLabel;
-
                 int diskAvail = Convert.ToInt32(gbDiskAvail / gbConversion);
+                int diskStart = Convert.ToInt32(gbDiskStart / gbConversion);
 
-                
-                Console.WriteLine();
-                Console.WriteLine(diskAvail);
+                Console.WriteLine(diskAvail + " gb");
+                Console.WriteLine(diskStart + " gb");
                 Console.WriteLine(drv.TotalFreeSpace + " Bytes");
                 Console.Write(drv.TotalSize + " Bytes");
+
                 Console.ReadKey();
             }
         }
